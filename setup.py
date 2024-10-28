2944ed7228dfa2376e62c91acbbaf317d03b6470
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-10-28 12:20:51 +0500

    Initialize project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Chrome plugin provides real-time sentiment analysis for comments on any open YouTube video, helping you quickly gauge viewer reactions. See the positive, negative, or neutral sentiment at a glance!',
+    author='Ubaid ur Rehman',
+    license='BSD-3',
+)
